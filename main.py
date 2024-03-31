1751c6fb2a23e1d4cbe1c3a09e9ccd08777a74bc
foadas <foad1381@gmail.com>
2024-03-31 22:07:07 +0430

    binning age

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import matplotlib.pyplot as plt
-
-radius = [1.0, 2.0, 3.0, 4.0]
-area = [3.14159, 12.56636, 28.27431, 50.26544]
-
-plt.plot(radius, area)
-plt.show()
+import pandas as pd
+import numpy as np
+max_age = 73
+min_age= 14
+range_age = max_age - min_age
+width = int(np.round(range_age/10))
+intervals = [age for age in range(min_age,max_age+width,width)]
+print(intervals)
